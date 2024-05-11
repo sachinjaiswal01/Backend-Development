2c4029b6df98e6d7adde595196ae19cb931cc35b
sachin jaiswal <119951251+sachinjaiswal01@users.noreply.github.com>
2024-05-11 18:13:12 +0000

    Added server files: one using Node.js and the other utilizing a bun.

---
+++ server-node.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port =  3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader( 'Content-Type', 'text/plain')
+        res.end("Hello ice tea");
+    }
+    else if(req.url === '/ice-tea') {
+        res.statusCode = 200;
+        res.setHeader( 'Content-Type', 'text/plain')
+        res.end("thanks for ordering ice tea ,its really hot");
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader( 'Content-Type', 'text/plain')
+        res.end("404 Not found");
+    }
+})
+
+server.listen(port ,hostname, () => {
+    console.log(`Server is listening oat http://${hostname}:${port}`)
+})
